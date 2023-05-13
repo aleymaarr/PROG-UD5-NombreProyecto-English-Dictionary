05eeb9902f5ab8cbbbf1c9fc270117bc41e18ae1
aleymarr <aleyba2002@gmail.com>
2023-05-13 10:34:23 +0100

    Feat(diccionario):Creando el metodo agregarPalabara

--- src/com/duolingo/dictionary/Diccionario.java
+++ src/com/duolingo/dictionary/Diccionario.java
@@ -3,8 +3,10 @@ package com.duolingo.dictionary;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Diccionario {
+
     private final Map<Character, Set<String>> mapaPalabras;
     private final Map<Character, Set<String>> mapaVerbosFrases;
 
@@ -12,4 +14,35 @@ public class Diccionario {
         mapaPalabras = new TreeMap<>();
         mapaVerbosFrases = new TreeMap<>();
     }
+
+    public boolean agregarPalabra(String palabra) {
+        if (palabra.contains(" ")) {
+            return agregarVerboFrasal(palabra);
+        }
+
+        Character inicial = Character.toLowerCase(palabra.charAt(0));
+        palabra = palabra.trim().toLowerCase();
+
+        if (!mapaPalabras.containsKey(inicial)) {
+            mapaPalabras.put(inicial, new TreeSet<>());
+        }
+
+        return mapaPalabras.get(inicial).add(palabra);
+    }
+
+    public boolean agregarPalabra(String palabra) {
+        if (palabra.contains(" ")) {
+            return agregarVerboFrasal(palabra);
+        }
+
+        Character inicial = Character.toLowerCase(palabra.charAt(0));
+        palabra = palabra.trim().toLowerCase();
+
+        if (!mapaPalabras.containsKey(inicial)) {
+            mapaPalabras.put(inicial, new TreeSet<>());
+        }
+
+        return mapaPalabras.get(inicial).add(palabra);
+    }
+
 }
