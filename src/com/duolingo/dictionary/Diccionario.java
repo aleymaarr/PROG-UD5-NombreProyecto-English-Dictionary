3164ef9bab36ac6d36044d21668dc5a09115ccd8
aleymarr <aleyba2002@gmail.com>
2023-05-13 10:32:49 +0100

    Feat(diccionario):Creando los metodos map y set para la clase diccionario

--- src/com/duolingo/dictionary/Diccionario.java
+++ src/com/duolingo/dictionary/Diccionario.java
@@ -1,4 +1,15 @@
 package com.duolingo.dictionary;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class Diccionario {
+    private final Map<Character, Set<String>> mapaPalabras;
+    private final Map<Character, Set<String>> mapaVerbosFrases;
+
+    public Diccionario() {
+        mapaPalabras = new TreeMap<>();
+        mapaVerbosFrases = new TreeMap<>();
+    }
 }
