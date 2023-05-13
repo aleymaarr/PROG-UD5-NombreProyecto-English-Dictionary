bec69cd8b233cb56a54cafe7ea6cc3d7a79d961b
aleymarr <aleyba2002@gmail.com>
2023-05-13 10:42:24 +0100

    Feat(diccionario):Creando el menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import com.duolingo.dictionary.Diccionario;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Diccionario diccionario = new Diccionario();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¡Bienvenido a la aplicación de Duolingo!");
 
     }
 }
